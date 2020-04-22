33f1d660e7c6c9075c0c3b355fa6792bc2d2c3f1
nicolasmq20 <62630822+nicolasmq20@users.noreply.github.com>
2020-04-22 13:59:36 -0300

    RESOLUÇÃO ALTERNATIVA QUESTÃO 3

--- P.I.py
+++ P.I.py
@@ -90,6 +90,24 @@ def classifica_lista(list):
 
 print(classifica_lista(test_list))
 
+#########################################----RESOLUÇÃO ALTERNATIVA QUESTÃO 3-----###################################################
+
+def classifica_lista(lista):
+    count = 0
+    for i in range(len(lista)):
+        if lista[i] != count:
+            count = lista[i]
+        else:
+            return 'nenhum'
+   
+    if len(lista) < 2 or (lista != sorted(lista, key=int) and lista != sorted(lista, key=int, reverse=True)):
+        return 'nenhum'
+    else:
+        if lista != sorted(lista, key=int):
+            return 'decrescente'
+        elif lista == sorted(lista, key=int):
+            return 'crescente'
+
 ###################################################----QUESTÃO 4-----###################################################
 
 
